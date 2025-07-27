06da4fbe45de5928dd0670a0b41295a99bcd654b
Harini <m.harini8460@gmail.com>
2025-07-27 11:35:55 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,45 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async(req, res,next) => {
- const { username, email, password } = req.body;
- const hashedPassword = bcryptjs.hashSync(password, 10);
- const newUser = new User({ username, email, password: hashedPassword});
- try {
-  await newUser.save()
-  res.status(201).json("User created successfully!");
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully!");
   } catch (error) {
-   next(error);
+    next(error);
   }
 };
-     
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid credentials!"));
+    }
+    // after the email and password is verified we need to authenticate the user
+    // for that we need to create a hash token that includes the email of the user or the id of the user  from the browser cookie and sent it to the server
+    // for the best package is jwt (json web token)
+    // based on the id created in the mongodb which is unique for every user we can create a token
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password:pass, ...rest}=validUser._doc; 
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
