738d973b6b01b06264e060f77a6bc3a9170d6982
Harini <m.harini8460@gmail.com>
2025-07-27 13:55:04 +0530

    add redux persist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
+
+
 // Importing necessary libraries and hooks from React and React Router
 export default function Signin() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);//useState is a hook that allows you to add state to functional components
+  const {loading, error} = useSelector((state) => state.user);
   const Navigate = useNavigate();//useNavigate is a hook that allows you to navigate to different routes in your application
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -17,7 +21,7 @@ console.log(formData);
 const handleSubmit = async(e) => {
   e.preventDefault();//this prevents refreshing the page when the form is submitted
   try {
-    setLoading(true);
+    dispatch(signInStart());
   const res =await fetch('/api/auth/signin',{
     method: 'POST',
     headers: {
@@ -28,16 +32,13 @@ const handleSubmit = async(e) => {
   const data = await res.json();
    console.log(data);
   if(data.success === false){
-    setLoading(false);
-    setError(data.message);    
+    dispatch(signInFailure(data.message));   
     return;
   }
-  setLoading(false);
-  setError(null);
+  dispatch(signInSuccess(data));
   Navigate('/');//this will redirect the user to the sign-in page after successful signup
   } catch (error) {
-    setLoading(false);
-    setError(error.message);
+    dispatch(signInFailure(error.message));  
   }
   };
 
@@ -48,12 +49,12 @@ const handleSubmit = async(e) => {
       <form onSubmit ={handleSubmit} className='flex flex-col gap-4 '>
          <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
           <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-          <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disables:opacity-80'>{loading?'Loading...': 'Sign Up' }</button>
+          <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disables:opacity-80'>{loading?'Loading...': 'Sign In' }</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Don't have an account?</p>
         <Link to ={"/sign-up"}>
-        <span className='text-blue-700'> Sign up</span>
+        <span className='text-blue-700'> Sign Up</span>
         </Link>
       </div>
       {error && <p className='text-red-500 mt-3'>{error}</p>}
